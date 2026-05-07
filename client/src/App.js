0c862e57269c2aae083ae8b330741f5c71f025d7
Khang <alexthewonder14@gmail.com>
2026-05-07 13:25:47 +0700

    when url is enter it now show the video title after 2s

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,11 @@ function App() {
   const [url, setUrl] = useState("");
   const [status, setStatus] = useState("idle");
   const [fileName, setFileName] = useState("");
+  const [videoTitle, setVideoTitle] = useState("");
 
   const handleDownload = async () => {
     if (!url) {
-      setStatus("error");
+      setStatus("pasue");
       return;
     }
 
@@ -33,6 +34,38 @@ function App() {
     }
   };
 
+  //extract the title from the url to show after use finish pasting / typing the url in
+  //add a dnbounce pattern in so that it will honly show the title of URL after a set ammount of time
+  useEffect(() => {
+    if (!url) {
+      return;
+    }
+    const timer = setTimeout(() => {
+      const fetchTitle = async () => {
+        try {
+          const res = await fetch("http://localhost:5000/get-title", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ url }),
+          });
+
+          const data = await res.json();
+
+          if (res.ok) {
+            setVideoTitle(data.title);
+            console.log("it worked dawg");
+          } else console.log("can't get the title bruhh");
+        } catch (error) {
+          setStatus("oyyy sth went wrong ehh");
+        }
+      };
+      fetchTitle();
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, [url]);
+
   useEffect(() => {
     console.log(status);
   }, [status]);
@@ -40,7 +73,7 @@ function App() {
   return (
     <div>
       <h1>Youtube to MP3 converter chigga</h1>
-      <p>Current URL value: {url}</p>
+      <p>Current URL value: {videoTitle}</p>
       {/* instead of showing the url, it will show the title of the video */}
 
       <input
@@ -55,7 +88,12 @@ function App() {
       {status === "idle" && <p>Enter a Youtube URL to get started.</p>}
       {status === "downloading" && <p>the video is downloading please wait</p>}
       {status === "done" && (
-        <p>the video has finished downloading {fileName}</p>
+        <div>
+          <p>Done your MP3 is ready to go buckaroo</p>
+          <a href={`http://localhost:5000/files/${fileName}`} download>
+            <button>Download MP3</button>
+          </a>
+        </div>
       )}
       {status === "error" && (
         <p>something went wrong please check the URL and try again</p>
--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ const { exec } = require("child_process");
 //your code like my server have its own cmd
 const path = require("path"); // help to find path to !!downloads!!
 const { v4: uuidv4 } = require("uuid");
+const { error } = require("console");
+const { stdout, stderr, title } = require("process");
 
 const app = express();
 //turning cors on
@@ -12,18 +14,22 @@ app.use(cors());
 //tell the server to expect json data from the UI
 app.use(express.json());
 
+app.use("/files", express.static(path.join(__dirname, "downloads")));
+
+//chỉ tải audio (nếu mà thêm cái lấy title ở đây thì nó ko đc do phải tải xong mới biết được tên video :)))))) không tải thì đm chỉ biết
+//cái url
 app.post("/download", (req, res) => {
   const { url } = req.body;
 
   //bỏ đi query parameter thuộc dạng list với index do hiện tại chức năng của server bây giờ là chỉ tải 1 vid đơn lẻ
   //chứ không phải tải playlist
-  const cleanUrl = new URL(url);
+  const cleanUrl = new URL(url); // url dạng object
   cleanUrl.searchParams.delete("list");
   cleanUrl.searchParams.delete("index");
   console.log("this is the cleanURL", cleanUrl);
 
   //lọc xong r thì nối lại link
-  const safeUrl = cleanUrl.toString();
+  const safeUrl = cleanUrl.toString(); // có toString() thì biến nó thành dạng string và chỉ lấy ra phần origin có link
   console.log("this is the safeURl", safeUrl);
 
   if (!url) {
@@ -32,6 +38,7 @@ app.post("/download", (req, res) => {
 
   const fileName = uuidv4();
   const outputPath = path.join(__dirname, "downloads", fileName);
+  console.log("file name: ", outputPath);
   //xiu test what will happended if i don't convert it to mp3?
   const command = `yt-dlp -x --audio-format mp3 -o "${outputPath}.%(ext)s" "${safeUrl}"`;
   //-x mean extract audio only
@@ -55,6 +62,38 @@ app.post("/download", (req, res) => {
       });
     },
   );
+});
+
+//chỉ dùng để lấy ra title
+app.post("/get-title", (req, res) => {
+  const { url } = req.body;
+
+  //bỏ đi query parameter thuộc dạng list với index do hiện tại chức năng của server bây giờ là chỉ tải 1 vid đơn lẻ
+  //chứ không phải tải playlist
+  const cleanUrl = new URL(url); // url dạng object
+  cleanUrl.searchParams.delete("list");
+  cleanUrl.searchParams.delete("index");
+
+  //lọc xong r thì nối lại link
+  const safeUrl = cleanUrl.toString(); // có toString() thì biến nó thành dạng string và chỉ lấy ra phần origin có link
+  const command = `yt-dlp -e "${safeUrl}"`;
+
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      return res.status(500).json({
+        error: "can't retrieve the vid title",
+        details: stderr,
+      });
+    }
+
+    console.log(`heyyy heyyy got the title ready bitch ass ${stdout.trim()}`);
+
+    //suecces
+    res.json({
+      message: `title retrieve successfully: ${stdout.trim()}`,
+      title: stdout.trim(),
+    });
+  });
 });
 
 app.listen(5000, () => {
