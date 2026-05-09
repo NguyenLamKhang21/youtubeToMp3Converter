a1d880487118fb2220ba24e1e8c7992d8b5ca747
Khang <alexthewonder14@gmail.com>
2026-05-09 16:31:55 +0700

    luu video voi ten la id cua video de tranh bat server tai lai file da co, bo uuid

--- server/index.js
+++ server/index.js
@@ -4,12 +4,12 @@ const { exec } = require("child_process");
 // use exec with child_process to run commands prompt commands from inside
 //your code like my server have its own cmd
 const path = require("path"); // help to find path to !!downloads!!
-const { v4: uuidv4 } = require("uuid");
 const { error } = require("console");
+const fs = require("fs");
 
 const app = express();
 //turning cors on
-app.use(cors());
+app.use(cors()); //allow all origins
 //tell the server to expect json data from the UI
 app.use(express.json());
 
@@ -29,17 +29,28 @@ app.post("/download", (req, res) => {
   const cleanUrl = new URL(url); // url dạng object
   cleanUrl.searchParams.delete("list");
   cleanUrl.searchParams.delete("index");
+  const videoId = cleanUrl.searchParams.get("v"); // lấy ra id định danh duy nhất của mỗi vidoe
   console.log("this is the cleanURL", cleanUrl);
 
   //lọc xong r thì nối lại link
   const safeUrl = cleanUrl.toString(); // có toString() thì biến nó thành dạng string và chỉ lấy ra phần origin có link
   console.log("this is the safeURl", safeUrl);
 
-  const fileName = uuidv4();
-  const outputPath = path.join(__dirname, "downloads", fileName);
-  console.log("file name: ", outputPath);
-  //xiu test what will happended if i don't convert it to mp3?
-  const command = `yt-dlp -x --audio-format mp3 -o "${outputPath}.%(ext)s" "${safeUrl}"`;
+  //lưu cái id đó thành fileName r lưu trên server
+
+  const filePath = path.join(__dirname, "downloads", `${videoId}`); //hieur la filePaht
+
+  if (fs.existsSync(filePath)) {
+    // File already exists! Skip downloading, just return the existing file
+    return res.json({
+      message: "ready to download",
+      file: `${videoId}.mp3`,
+    });
+  }
+
+  console.log("file name: ", filePath);
+
+  const command = `yt-dlp -x --audio-format mp3 -o "${filePath}.%(ext)s" "${safeUrl}"`;
   //-x mean extract audio only
   //then convert it into mp3
 
@@ -54,10 +65,10 @@ app.post("/download", (req, res) => {
         });
       }
 
-      // success — send the file info back (bao gồm cả title)
+      // success — send the file info back
       res.json({
         message: "Download successful",
-        file: `${fileName}.mp3`,
+        file: `${videoId}.mp3`,
       });
     },
   );
@@ -81,23 +92,32 @@ app.post("/get-title", (req, res) => {
   const safeUrl = cleanUrl.toString(); // có toString() thì biến nó thành dạng string và chỉ lấy ra phần origin có link
   const command = `yt-dlp -e "${safeUrl}"`;
 
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      return res.status(500).json({
-        error: "can't retrieve the vid title",
-        details: stderr,
+  exec(
+    command,
+    { encoding: "utf-8", env: { ...process.env, PYTHONIOENCODING: "utf-8" } },
+    (error, stdout, stderr) => {
+      if (error) {
+        return res.status(500).json({
+          error: "can't retrieve the vid title",
+          details: stderr,
+        });
+      }
+
+      console.log(
+        `heyyy heyyy got the title ready bitch ass: ${stdout.trim()}`,
+      );
+
+      //suecces
+      res.json({
+        message: `title retrieve successfully: ${stdout.trim()}`,
+        title: stdout.trim(),
       });
-    }
-
-    console.log(`heyyy heyyy got the title ready bitch ass ${stdout.trim()}`);
-
-    //suecces
-    res.json({
-      message: `title retrieve successfully: ${stdout.trim()}`,
-      title: stdout.trim(),
-    });
-  });
+    },
+  );
 });
+//dùng lệnh dưới đây để post lấy thumbnail, nhớ phải lọc cái url trc không thì nó tải hết playlist đấy
+//yt-dlp --write-thumbnail --skip-download -o "E:\HobbyProject\youtubeToMp3Converter\server\test thumbnail\%(title)s.%(ext)s" "https://www.youtube.com/watch?v=lHLJkdqnhak
+app.p;
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
